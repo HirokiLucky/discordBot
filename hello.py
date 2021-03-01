44092de06512476ed064a4c80da984aaad8e31c4
LAPTOP-P687UVBT\81808 <nagadon0424@icloud.com>
2021-03-01 14:57:06 +0900

    hello.py のファイルが間違っていたので変えました

--- hello.py
+++ hello.py
@@ -1,3 +1,26 @@
-import discord.py
+import discord
 
-print("a")
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'TOKEN'
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「おはよう」が返る処理
+    if message.content == 'おはよう':
+        await message.channel.send('おはよう')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
